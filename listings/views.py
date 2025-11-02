1819cfce7a8815b961e424428b6a48c9522f6db7
Moana <122973439+MoanaZulu@users.noreply.github.com>
2025-11-02 22:17:51 +0200

    Update views.py

--- listings/views.py
+++ listings/views.py
@@ -4,3 +4,8 @@ from rest_framework.response import Response
 class HelloWorld(APIView):
     def get(self, request):
         return Response({"message": "Hello, ALX!"})
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Welcome to Listings!")
