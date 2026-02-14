fc82ef86ca3a11cd8900b79442e9ccf3c1adc505
Moana <missn.hadebe@gmail.com>
2026-02-14 12:08:33 +0200

    Fix urls.py with valid urlpatterns

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('listings/', include('listings.urls')),  # connect your app
+]
+
+
+
+
+
+
+from django.contrib import admin
+from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
